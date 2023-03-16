fed2187c62a8578b6ea372c91fea1812aedc200f
Dmitriy Kostyuk <DISwered@gmail.com>
2023-03-16 12:36:47 +0300

    Implemented:     -CONTINUE button

--- src/main/java/com/example/calculatorwithinterface/ResultController.java
+++ src/main/java/com/example/calculatorwithinterface/ResultController.java
@@ -3,6 +3,7 @@ package com.example.calculatorwithinterface;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -22,7 +23,12 @@ public class ResultController {
         resultLabel.setText(input + "=" + result);
     }
 
-//    public void Continue(ActionEvent event) throws IOException {}
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("CalculatorScene.fxml"));
-
+    public void Continue(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("CalculatorScene.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
