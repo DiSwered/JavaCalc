afff7e5d3a1c6508ee9b8c4ccc04453eabff6ed2
Dmitriy Kostyuk <DISwered@gmail.com>
2023-03-16 13:43:46 +0300

    Implemented:     -Buttons functionality

--- src/main/java/com/example/calculatorwithinterface/Controller.java
+++ src/main/java/com/example/calculatorwithinterface/Controller.java
@@ -16,11 +16,12 @@ public class Controller {
     @FXML
     TextField textField;
 
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void Solve(ActionEvent event) throws IOException {
+    public void solve(ActionEvent event) throws IOException {
         String inputField = textField.getText();
         Main main = new Main();
 
@@ -36,8 +37,24 @@ public class Controller {
         stage.show();
     }
 
-//    public String addToInput(String inputField) {
-//        switch ()
-//        return inputField;
-//    }
+    public void addToInput(ActionEvent event) {
+        String inputField = textField.getText();
+        switch (((Node)event.getSource()).getId()) {
+            case "buttonOne" -> textField.setText(inputField + "1");
+            case "buttonTwo" -> textField.setText(inputField + "2");
+            case "buttonThree" -> textField.setText(inputField + "3");
+            case "buttonFour" -> textField.setText(inputField + "4");
+            case "buttonFive" -> textField.setText(inputField + "5");
+            case "buttonSix" -> textField.setText(inputField + "6");
+            case "buttonSeven" -> textField.setText(inputField + "7");
+            case "buttonEight" -> textField.setText(inputField + "8");
+            case "buttonNine" -> textField.setText(inputField + "9");
+            case "buttonZero" -> textField.setText(inputField + "0");
+            case "buttonPlus" -> textField.setText(inputField + "+");
+            case "buttonMinus" -> textField.setText(inputField + "-");
+            case "buttonMult" -> textField.setText(inputField + "*");
+            case "buttonDiv" -> textField.setText(inputField + "/");
+            case "buttonDEL" -> textField.setText(inputField.substring(0, inputField.length() - 1));
+        }
+    }
 }
